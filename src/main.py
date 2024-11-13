d6f926bfffd5ddaa1b2a4257624080be342736fa
Николай Перминов <kolya-perminov@ya.ru>
2024-11-14 00:59:55 +0500

    Автоматический перезапуск потоков

--- src/main.py
+++ src/main.py
@@ -1,23 +1,35 @@
 import importlib
 from time import sleep
+from typing import List
 from bot import bot
 from os import listdir
 from os.path import isfile, join
 from threading import Thread
 import logging
 import warnings
+
+
+class RestartableThread(Thread):
+    def __init__(self, *args, **kwargs):
+        self._args, self._kwargs = args, kwargs
+        super().__init__(*args, **kwargs)
+
+    def clone(self):
+        return RestartableThread(*self._args, **self._kwargs)
+
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 secrets_path = "bot_secrets"
 
 files = [f for f in listdir(secrets_path) if isfile(join(secrets_path, f)) and f != 'sample.py' and f[-3:] == '.py']
-threads = []
+threads: List[RestartableThread] = []
 
 for file in files:
     secret_module = importlib.import_module(secrets_path + "." + file[:-3])
     print(secret_module.__name__)
-    threads.append(Thread(
+    threads.append(RestartableThread(
         target=bot,
         args=(secret_module, len(threads),),
         daemon=True
